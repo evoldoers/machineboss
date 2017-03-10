e3b04795ea6f1b532d268bb08db208eae433dde3
Ian Holmes <github@yam.biowiki.org>
2017-03-09 16:52:02 -0800

    trimmed params schema

--- src/params.cpp
+++ src/params.cpp
@@ -12,17 +12,16 @@ void Params::readJson (istream& in) {
 void Params::readJson (const json& pj) {
   MachineSchema::validateOrDie ("params", pj);
   param.clear();
-  const auto& p = pj.at("param");
-  for (auto iter = p.begin(); iter != p.end(); ++iter)
+  for (auto iter = pj.begin(); iter != pj.end(); ++iter)
     param[iter.key()] = iter.value().get<double>();
 }
 
 void Params::writeJson (ostream& out) const {
-  out << "{\"param\":{";
+  out << "{";
   size_t n = 0;
   for (auto& pv: param)
     out << (n++ ? "," : "") << "\"" << pv.first << "\":" << pv.second;
-  out << "}}";
+  out << "}";
 }
 
 Params Params::fromJson (istream& in) {
