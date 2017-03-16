05fb1e999b0453699466064afae55daa27b8c035
Ian Holmes <github@yam.biowiki.org>
2017-03-16 15:08:14 -0700

    genewise-like components

--- node/add-intron-placeholders.js
+++ node/add-intron-placeholders.js
@@ -4,6 +4,7 @@ var prot = "ACDEFGHIKLMNPQRSTVWY".split("")
 
 var machine = { state: [{ id: "S",
                           trans: prot.reduce ((t,c) => t.concat([
+                            { to: "E" },
                             { in: c, out: c, to: "S", weight: {"-":[true,"intron"]} },
                             { in: c, to: "IBB", weight: {"/":["intron",3]} },
                             { in: c, to: "BIB", weight: {"/":["intron",3]} },
@@ -16,6 +17,7 @@ var machine = { state: [{ id: "S",
                         { id: "IB", trans: [{ out: "intron", to: "B" }] },
                         { id: "BBI", trans: [{ out: "base", to: "BI" }] },
                         { id: "BI", trans: [{ out: "base", to: "I" }] },
-                        { id: "I", trans: [{ out: "intron", to: "S" }] }] }
+                        { id: "I", trans: [{ out: "intron", to: "S" }] },
+			{ id: "E" }] }
 
 console.log (JSON.stringify (machine))
--- node/simple-introns.js
+++ node/simple-introns.js
@@ -3,12 +3,14 @@
 var prot = "ACDEFGHIKLMNPQRSTVWY".split("")
 
 var machine = { state: [{ id: "S",
-                          trans: prot.map ((c) => [{ in: c, out: c, to: "S" }])
+                          trans: prot.map ((c) => { return { in: c, out: c, to: "S" } })
                           .concat ([{ in: "base", out: "base", to: "S"},
-                                    { in: "intron", out: "G", to: "donor" }]) },
+                                    { in: "intron", out: "G", to: "donor" },
+				    { to: "E" }]) },
                         { id: "donor", trans: [{ out: "T", to: "intron" }] },
                         { id: "intron", trans: [{ out: "base", to: "intron", weight: "extendIntron" },
                                                 { out: "A", to: "acceptor", weight: {"-":[true,"extendIntron"]} }] },
-                        { id: "acceptor", trans: [{ out: "G", to: "S" }] }] }
+                        { id: "acceptor", trans: [{ out: "G", to: "S" }] },
+			{ id: "E" }] }
 
 console.log (JSON.stringify (machine))
