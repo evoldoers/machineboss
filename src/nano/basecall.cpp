d07a0689a3e930fbf8010c89b91cddf157635d01
Ian Holmes <ihh@berkeley.edu>
2017-04-26 22:27:52 -0700

    fixed toposort

--- src/nano/basecall.cpp
+++ src/nano/basecall.cpp
@@ -132,6 +132,6 @@ void BaseCallingMachine::init (const string& alph, SeqIdx len, int cpts) {
     for (auto c: alph)
       end.trans.push_back (MachineTransition (string(1,c), string(), kmerStart (stringToKmer (suffix + c, alph)), WeightExpr (condFreqLabel (suffix, c))));
     state[startState()].trans.push_back (MachineTransition (string(), string(), si, WeightExpr(1. / (double) nk)));
-    end.trans.push_back (MachineTransition (string(), string(), si, WeightExpr(1.)));
+    end.trans.push_back (MachineTransition (string(), string(), nStates() - 1, WeightExpr(1.)));
   }
 }
