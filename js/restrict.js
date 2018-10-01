b2232cef38f7f1bba4f27ec4aa5a4ace625cae60
Ian Holmes <ihh@berkeley.edu>
2018-09-30 19:17:13 -0700

    restriction enzyme avoidance

--- js/restrict.js
+++ js/restrict.js
@@ -9,12 +9,13 @@ var fs = require('fs'),
 // parse command-line options
 var opt = getopt.create([
   ['m' , 'motif=SEQ'        , 'specify motif [ACGTN]+'],
+  ['f' , 'forward'          , 'do not check reverse strand'],
   ['h' , 'help'             , 'display this help message']
 ])              // create Getopt instance
     .bindHelp()     // bind option 'help' to default action
     .parseSystem() // parse command line
 
-var motif = opt.options.motif
+var motif = opt.options.motif, forward = opt.options.forward
 if (!motif)
   throw new Error ("please specify a motif")
 var motifSeq = motif.toUpperCase().split('')
@@ -22,19 +23,35 @@ var motifSeq = motif.toUpperCase().split('')
 // The state space is indexed (M[1],M[2]...M[motif.length-1])
 // where M[K] is a binary variable that is true iff the previously-emitted K symbols match the first K characters of the motif.
 // We only need to keep 2^(motif.length-1) states because the last bit is never allowed to be 1.
-const alphabet = [ "A", "C", "G", "T" ]
+const alphabet = [ "A", "C", "G", "T" ], complement = { A:"T", C:"G", G:"C", T:"A" }
 const iupac = { A: "A", C: "C", G: "G", T: "T", W: "AT", S: "CG", M: "AC", K: "GT", R: "AG", Y: "CT", B: "CGT", D: "AGT", H: "ACT", V: "ACG", N: "ACGT" }
-const initTuple = motifSeq.slice(1).fill(false)
+const tupleChars = [ '_', 'f', 'r', 'b' ], tupleCharIndex = { _:0, f:1, r:2, b:3 }
+const initTuple = motifSeq.slice(1).fill(0)
+const tupleRegex = new RegExp ('([frb])([0-9]+)','g')
 function string2tuple (state) {
-  return state.split('').map (function (c) { return c === '1' ? true : false })
+  var tuple = initTuple.slice(0)
+  var match
+  while (match = tupleRegex.exec (state)) {
+    var flags = tupleCharIndex[match[1]], pos = parseInt(match[2]) - 1
+    tuple[pos] = flags
+  }
+  return tuple
 }
 function tuple2string (tuple) {
-  return tuple.map (function (bit) { return bit ? '1' : '0' }).join('')
+  return tuple.map (function (flags, pos) {
+    return flags ? (tupleChars[flags] + (pos+1)) : ''
+  }).join('') || 'start'
 }
 function nextTuple (tuple, tok) {
-  var t = [true].concat(tuple).map (function (prevMatch, pos) {
-    return prevMatch && iupac[motifSeq[pos]].indexOf(tok) >= 0
+  var t = [forward ? 1 : 3].concat(tuple).map (function (prevMatch, pos) {
+    var prevFwdMatch = (prevMatch & 1) ? true : false
+    var prevRevMatch = (prevMatch & 2) ? true : false
+    var fwdMatch = prevFwdMatch && iupac[motifSeq[pos]].indexOf(tok) >= 0
+    var revMatch = prevRevMatch && iupac[motifSeq[motifSeq.length-1-pos]].indexOf(complement[tok]) >= 0
+//    console.warn('tuple',tuple,'pos',pos,'prevMatch',prevMatch,'tok',tok,'iupac',iupac[motifSeq[pos]],'fwd',fwdMatch,'rev',revMatch)
+    return (fwdMatch ? 1 : 0) | (revMatch ? 2 : 0)
   })
+//  console.warn('nextTuple',tuple,tok,t)
   var valid = !t.pop()
   return { tok: tok, next: t, valid: valid }
 }
@@ -54,12 +71,12 @@ while (tupleQueue.length) {
         tupleQueue.push (info.next)
       return { in: info.tok, out: info.tok, to: dest }
     })
-    states[id2index[id]] = { id: id, trans: trans }
+    states[id2index[id]] = { id: id, trans: trans.concat ([{ to: 'end' }]) }
   }
 }
 
 var model = {
-  state: states
+  state: states.concat ([{ id: 'end', trans: [] }])
 }
 
 console.log (JSON.stringify (model, null, 2))
