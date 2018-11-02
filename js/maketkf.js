7a7807329b214bfdaff937832b094374ed66bd31
Ian Holmes <ihh@berkeley.edu>
2018-11-01 17:49:12 -0700

    ok

--- js/maketkf.js
+++ js/maketkf.js
@@ -4,14 +4,20 @@ var fs = require('fs'),
     Getopt = require('node-getopt')
 
 var getopt = Getopt.create([
-    ['a' , 'alphabet=STRING'  , 'alphabet'],
-    ['h' , 'help'             , 'display this help message']
+  ['a' , 'alphabet=CHARS', 'specify alphabet characters explicitly'],
+  ['d' , 'dna', 'DNA alphabet'],
+  ['r' , 'rna', 'RNA alphabet'],
+  ['p' , 'protein', 'Protein alphabet'],
+  ['h' , 'help', 'display this help message']
 ])              // create Getopt instance
 .bindHelp()     // bind option 'help' to default action
 var opt = getopt.parseSystem() // parse command line
 
+var alphabets = { dna: 'ACGT', rna: 'ACGU', protein: 'ACDEFGHIKLMNPQRSTVWY' }
+Object.keys(alphabets).forEach (function (alphName) { if (opt.options[alphName]) opt.options.alphabet = alphabets[alphName] })
+
 opt.options.alphabet || console.warn ('No alphabet specified, using DNA')
-const alph = (opt.options.alphabet || 'ACGT').split("")
+const alph = (opt.options.alphabet || alphabets.dna).split("")
 
 function transitions (inLabel, outLabel, dest) {
     return alph.map (function (c) {
@@ -51,35 +57,36 @@ function eqm (c) {
 }
 
 var lambda = "lambda", mu = "mu", t = "t"
-function insOpen() {
-}
 
-var alpha = "alpha", beta = "beta", gamma = "gamma", rho = "rho", kappa = "kappa"
+var pSurvived = "pSurvived", pChildless = "pChildless", pIns = "pIns", pExtinct = "pExtinct", beta = "beta", pLonger = "pLonger"
+var startState = "START", deleteState = "DELETE", waitState = "WAIT", insertState = "INSERT", endState = "END"
+
 var machine = { state:
-                [{id: "S",
-		  trans: [{to: "I", weight: beta },
-			  {to: "W", weight: not(beta) }]},
-		 {id: "D",
-		  trans: [{to: "I", weight: gamma },
-			  {to: "W", weight: not(gamma) }]},
-                 {id: "W",
+                [{id: startState,
+		  trans: [{to: insertState, weight: pIns },
+			  {to: waitState, weight: not(pIns) }]},
+		 {id: deleteState,
+		  trans: [{to: insertState, weight: not(pExtinct) },
+			  {to: waitState, weight: pExtinct }]},
+                 {id: waitState,
 		  trans: alph.reduce ((t,c) => t.concat (alph.map ((d) => {
-                    return { in: c, out: d, to: "S", weight: times (alpha, sub(c,d)) }
+                    return { in: c, out: d, to: startState, weight: times (pSurvived, sub(c,d)) }
                   })), []).concat (alph.map ((c) => {
-                    return { in: c, to: "D", weight: not(alpha) }
-                  })).concat ([{to: "E" }])},
-                 {id: "I",
-		  trans: alph.map ((c) => {return { out: c, to: "S", weight: eqm(c) } })},
-		 {id: "E"}],
+                    return { in: c, to: deleteState, weight: not(pSurvived) }
+                  })).concat ([{to: endState }])},
+                 {id: insertState,
+		  trans: alph.map ((c) => {return { out: c, to: startState, weight: eqm(c) } })},
+		 {id: endState}],
                 defs: {
-                  kappa: div(lambda,mu),
-                  rho: exp(times(t,minus(lambda,mu))),
-                  alpha: exp(times(-1,times(mu,t))),
-                  beta: times(kappa,div(not(rho),not(times(kappa,rho)))),
-                  gamma: not(div(beta,times(not(alpha),kappa)))
+                  pSurvived: exp(times(-1,times(mu,t))),
+                  pChildless: exp(times(-1,times(lambda,t))),
+                  beta: div(pSurvived,pChildless),
+                  pLonger: div(lambda,mu),
+                  pIns: div(times(pLonger,not(beta)),not(times(pLonger,beta))),
+                  pExtinct: div(pIns,times(pLonger,not(pSurvived)))
                 },
-                cons: { norm: [alph.map ((c) => "eqm"+c)]
-                        .concat (alph.map ((c) => alph.map ((d)=>"sub"+c+d))),
+                cons: { norm: [alph.map (eqm)]
+                        .concat (alph.map ((c) => alph.map (sub.bind (null, c)))),
                         rate: [lambda, mu, t] } }
 
 console.log (JSON.stringify (machine, null, 2))
