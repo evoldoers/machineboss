db9b1e26ace8d12acdf9c4d1ac8c24fa41d23a39
Ian Holmes <ihholmes@gmail.com>
2019-02-22 10:59:33 -0800

    testing assembly I/O

--- src/assembly.cpp
+++ src/assembly.cpp
@@ -57,29 +57,38 @@ void Assembly::validate() const {
   vguard<InputSymbol> seq;
   seq.reserve (generatorPath.trans.size());
   StateIndex gState = generator.startState();
+  size_t ngTrans = 0;
   for (auto t: generatorPath.trans) {
+    ++ngTrans;
     const MachineState gms = generator.state[gState];
-    Assert (t < gms.trans.size(), "Generator transition out of range");
+    Assert (t < gms.trans.size(), "Generator path transition %u, source state %u: Transition %u out of range", ngTrans, gState, t);
     const MachineTransition gmt = gms.getTransition (t);
     if (!gmt.outputEmpty())
       seq.push_back (gmt.out);
     gState = gmt.dest;
   }
+  Assert (gState == generator.endState(), "Generator path finishes in state %u, not the generator machine's end state which is %u", gState, generator.endState());
 
+  size_t nPath = 0;
   for (auto errorPath: errorPaths) {
-    Assert (errorPath.start <= seq.size(), "Alignment start index out of range");
+    ++nPath;
+    Assert (errorPath.start <= seq.size(), "Alignment %u: start position %u out of range for sequence length %u", nPath, errorPath.start, seq.size());
     InputIndex ePos = errorPath.start;
     StateIndex eState = error.startState();
+    size_t neTrans = 0;
     for (auto t: errorPath.trans) {
+      ++neTrans;
       const MachineState ems = error.state[eState];
-      Assert (t < ems.trans.size(), "Error transition out of range");
+      Assert (t < ems.trans.size(), "Alignment %u, transition %u, source state %u: Transition %u out of range", nPath, neTrans, eState, t);
       const MachineTransition emt = ems.getTransition (t);
       if (!emt.inputEmpty()) {
-	Assert (ePos < seq.size(), "Alignment column out of range");
-	Assert (emt.in == seq[ePos++], "Read/sequence mismatch");
+	Assert (ePos < seq.size(), "Alignment %u, transition %u, source state %u: Alignment passed end of sequence", nPath, neTrans, eState);
+	Assert (emt.in == seq[ePos], "Alignment %u, transition %u, source state %u: Transition input label (%s) does not match sequence symbol (%s)", nPath, neTrans, eState, emt.in.c_str(), seq[ePos].c_str());
+	++ePos;
       }
       eState = emt.dest;
     }
+    Assert (eState == error.endState(), "Alignment %u: Alignment path finishes in state %u, not the error machine's end state which is %u", nPath, eState, error.endState());
   }
 }
 
