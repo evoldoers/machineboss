3847f3f020f9269ecb6dd29b3a9827562dd9177d
Ian Holmes <ihh@berkeley.edu>
2020-02-04 20:50:26 -0800

    note on PEG grammar for regexes

--- src/parseregex.cpp
+++ src/parseregex.cpp
@@ -18,5 +18,24 @@ string RegexParser::alphabet() const {
 
 Machine RegexParser::parse (const string& str) const {
   // TODO: WRITE ME
+
+  /* PEG grammar:
+REGEX <- BEGIN_ANCHOR SYMBOL* END_ANCHOR
+BEGIN_ANCHOR <- '^'?
+END_ANCHOR <- '$'?
+SYMBOL <- NEGATED_CHAR_CLASS / CHAR_CLASS / ALTERNATION / .
+CHAR_CLASS <- '[' CHAR+ ']'
+NEGATED_CHAR_CLASS <- '[' '^' CHAR+ ']'
+CHAR <- DIGIT_CHAR / WHITE_CHAR / NONWHITE_CHAR / ANY_CHAR / ESCAPED_CHAR / (!']' .)
+DIGIT_CHAR <- '\\' 'd'
+WHITE_CHAR <- '\\' 's'
+NONWHITE_CHAR <- '\\' 'S'
+ANY_CHAR <- '.'
+ESCAPED_CHAR <- '\\' .
+ALTERNATION <- '(' ALT_OPTION ('|' ALT_OPTION)+ ')'
+ALT_OPTION <- ALT_SYMBOL*
+ALT_SYMBOL <- !'|' !')' SYMBOL
+  */
+
   return Machine::null();
 }
