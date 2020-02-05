9d657616227f3c9917a4f0b21c4737a6c2241c85
Ian Holmes <ihh@berkeley.edu>
2020-02-04 21:56:13 -0800

    more on PEG grammar for regexes

--- src/parseregex.cpp
+++ src/parseregex.cpp
@@ -20,21 +20,25 @@ Machine RegexParser::parse (const string& str) const {
   // TODO: WRITE ME
 
   /* PEG grammar:
+
 REGEX <- BEGIN_ANCHOR SYMBOL* END_ANCHOR
 BEGIN_ANCHOR <- '^'?
 END_ANCHOR <- '$'?
 SYMBOL <- NEGATED_CHAR_CLASS / CHAR_CLASS / ALTERNATION / .
 CHAR_CLASS <- '[' CHAR+ ']'
 NEGATED_CHAR_CLASS <- '[' '^' CHAR+ ']'
-CHAR <- DIGIT_CHAR / WHITE_CHAR / NONWHITE_CHAR / ANY_CHAR / ESCAPED_CHAR / (!']' .)
+CHAR <- DIGIT_CHAR / WHITE_CHAR / NONWHITE_CHAR / WILD_CHAR / CHAR_RANGE / SINGLE_CHAR
 DIGIT_CHAR <- '\\' 'd'
 WHITE_CHAR <- '\\' 's'
 NONWHITE_CHAR <- '\\' 'S'
-ANY_CHAR <- '.'
+WILD_CHAR <- '.'
+CHAR_RANGE <- SINGLE_CHAR '-' SINGLE_CHAR
+SINGLE_CHAR <- ESCAPED_CHAR / (!']' .)
 ESCAPED_CHAR <- '\\' .
 ALTERNATION <- '(' ALT_OPTION ('|' ALT_OPTION)+ ')'
 ALT_OPTION <- ALT_SYMBOL*
 ALT_SYMBOL <- !'|' !')' SYMBOL
+
   */
 
   return Machine::null();
